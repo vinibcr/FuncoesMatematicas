06b63de2f71e400255f3321fd34f9fbe39b449b9
Vinícius Crepaldi <105659585+vinibcr@users.noreply.github.com>
2024-08-23 08:52:27 -0300

    Add files via upload

---
+++ Aguardando aprovação Caixa.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+from tkinter import messagebox
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def enviar_email():
+    # Obter os valores dos campos de entrada
+    chamado_numero = chamado_numero_entry.get()
+    email_destino = email_destino_entry.get()
+
+    # Corpo do e-mail com a formatação correta
+    email_body = f"""\
+    https://sesisenaisp.atlassian.net/browse/CS-{chamado_numero}
+
+    Prezado, tudo bem?
+    
+    Gostaria de te avisar que tem o chamado ({chamado_numero}) aguardando por aprovação, e sem ela o chamado permanecerá sem as devidas tratativas.
+    
+    Se tiver alguma dúvida de como proceder com essa aprovação, por favor, liga para a gente que eu te explico como fazer (Tel: 3254-3525).
+    
+    Poderia responder de acordo no Email em questão para aprovar o mesmo, ou acessar o link para aprovar.
+    
+    Atenciosamente,
+    
+    Vinícius Crepaldi.
+    """
+    
+    # Configurações do servidor de e-mail corporativo
+    smtp_server = 'smtp.office365.com'
+    smtp_port = 587
+    email_remetente = 'centraldeservicos@digisystem.com.br'
+    senha_remetente = 'digisystem@2024'
+
+    # Configuração da mensagem
+    msg = MIMEMultipart()
+    msg['From'] = email_remetente
+    msg['To'] = email_destino
+    msg['Subject'] = f'Solicitação de acesso ({chamado_numero})'
+    
+    # Corpo do e-mail
+    msg.attach(MIMEText(email_body, 'plain'))
+    
+    # Enviar o e-mail
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()  # Ativa a segurança
+        server.login(email_remetente, senha_remetente)
+        server.send_message(msg)
+        server.quit()
+        messagebox.showinfo("Sucesso", "E-mail enviado com sucesso!")
+    except Exception as e:
+        messagebox.showerror("Erro", f"Falha ao enviar e-mail: {e}")
+
+# Interface gráfica utilizando tkinter
+root = tk.Tk()
+root.title("Enviar E-mail") 
+root.geometry("200x150")
+
+# Campos de entrada
+tk.Label(root, text="Número do Chamado:").pack()
+chamado_numero_entry = tk.Entry(root)
+chamado_numero_entry.pack()
+
+tk.Label(root, text="E-mail de Destino:").pack()
+email_destino_entry = tk.Entry(root)
+email_destino_entry.pack()
+
+# Botão enviar
+enviar_button = tk.Button(root, text="Enviar", command=enviar_email)
+enviar_button.pack(pady=10)
+
+root.mainloop()
